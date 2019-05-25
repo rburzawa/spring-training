766f100252582a4e5d1d6b1cfdd4173052a12af3
Rafal <rburzawa@gmail.com>
2019-05-25 14:40:13 +0200

    first commit

---
+++ src/main/java/pl/sda/springtraining/springtraining/SpringTrainingApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.springtraining.springtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTrainingApplication.class, args);
+    }
+
+}
